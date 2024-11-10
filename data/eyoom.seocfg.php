07d3c2b506c83dedba7941050b350ff240cee8da
Bilal <bm@fabricdata.com>
2024-11-10 18:17:21 +0300

    Project Setup

---
+++ data/eyoom.seocfg.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined('_EYOOM_')) exit;
+$seocfg = array(
+	"mt_verification" => " &lt;meta name=\\&quot;google-site-verification\\&quot; content=\\&quot;verification code\\&quot;/&gt;",
+	"mt_title" => "",
+	"mt_keywords" => "",
+	"mt_description" => "",
+	"mt_robots" => "",
+	"mt_author" => "",
+	"mt_publisher" => "",
+	"mt_img_url" => "",
+);
